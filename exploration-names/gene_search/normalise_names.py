f2bccce912d6ddd9c8ea5a86978fdbd851c42d7c
yaliouat <yasmine.aliouat@etu.univ-lyon1.fr>
2026-02-25 22:46:07 +0100

    Version fonctionnelle de normalise_names.py

--- exploration-names/gene_search/normalise_names.py
+++ exploration-names/gene_search/normalise_names.py
@@ -14,39 +14,62 @@ def gene_versions(adata):
     adata.var["has_versions"]=names.str.contains(r"\.", regex=True) # a plus qu'une version ou pas
     return adata
 
+def make_groups(annot: pd.DataFrame)-> pd.DataFrame:
+    """
+    Cette fonction permet de regrouper les veleurs différente pour un 
+    même gene_id.
+    """
+    return(annot.groupby("ensembl_gene_id", sort=False)
+    .agg(lambda x: "|". join(x.dropna().astype(str).unique()))
+    ) #Les noms seront séparer par un '|'
+
+def mapping(adata, data, grouped_names, mapping_dict):
+    """
+    Cette fonction permet de faire la correspondance entre le nom de la 
+    colonne dans les AnnData et le nom présent dans BioMart
+    """
+    for biomart_col, out_col in mapping_dict.items():
+        if biomart_col in grouped_names: #Car BioMart ne renvoie pas toujours toutes les colonnes
+            adata.var[out_col]=data.map(grouped_names[biomart_col]) 
+    return adata
+
+
 def add_biomart_names(adata, organism="hsapiens"):
     """
     Cette fonction ajoute plusierurs colonnes si disponible via BioMart:
     gene_symbol; hgnc_symbol; hgnc_id, alias_symbol, entrezgene_id (NCBI)
     refseq_mrna, refseq_peptide, uniprotswissprot, uniprotsptrembl
+    Séparer en plusieurs requetes car BioMart permet la rechrche de 4 attribus à la fois.
     """
     if "gene_ids" not in adata.var.columns:
         raise ValueError ("gene_ids manquant")  #Si on utilise un autre fichier
 
-    columns= [
+    ids= adata.var["gene_ids"].astype(str)
+
+    # Requete 1:
+    cols1= [
         "ensembl_gene_id", "external_gene_name", "hgnc_symbol", "hgnc_id",
-        "external_synonym", "entrezgene_id", "refseq_mrna", "refseq_peptide", 
-        #"uniprotswissprot", "uniprotsptrembl",
     ]
+    grp1= make_groups(biomart_annotations(organism, cols1))
+    adata= mapping(adata, ids, grp1, {"external_gene_name": "gene_symbol", 
+    "hgnc_symbol":"hgnc_symbol" , "hgnc_id": "hgnc_id"})
 
-    annot=biomart_annotations(organism, columns)
+    # Requete 2:
+    cols2= [
+        "ensembl_gene_id", "external_synonym", "entrezgene_id",
+    "refseq_mrna",
+    ]
+    grp2= make_groups(biomart_annotations(organism, cols2))
+    adata= mapping(adata, ids, grp2, {
+    "external_synonym":"alias_symbol" , "entrezgene_id": "NCBI_symbol", "refseq_mrna": "refseq_mrna"})
 
-    existant= [col for col in columns if col in annot.columns ] #Ne garder que les colonnes existantes
-
-    grouped_annot= (annot.groupby("ensembl_gene_id").agg(lambda x: "|".join (x.dropna().astype(str).unique()))) #Certain Ids ensemble pointe vers plusiers noms
-    #adata.var["alias_symbol"]= adata.var["gene_ids"].map(grouped_annot["alias_symbol"])
-
-    rename={
-        "external_gene_name": "gene_symbol", "entrezgene_id":"NCBI_gene_id",
-        "uniprotswissprot":"uniprot_swissprot", "uniprotsptrembl":"uniprot_trembl",
-
-    }
-
-    for column in existant:
-        if column == "ensembl_gene_id":
-            continue
-        out_column= rename.get(column, column)
-        adata.var[out_column]=adata.var["gene_ids"].map(grouped_annot[column])
+    # Requete 3:
+    cols3= [
+        "ensembl_gene_id", "uniprotswissprot", "uniprotsptrembl"
+    ]
+    grp3= make_groups(biomart_annotations(organism, cols3))
+    adata= mapping(adata, ids, grp3, {
+    "uniprotswissprot": "uniprot_swissprot", "uniprotsptrembl": "uniprot_sptrembl"})
 
     return adata
 
@@ -80,13 +103,15 @@ if __name__=="__main__":
         adata= add_biomart_names(adata, organism=args.organism)
 
     new_columns= [col for col in [
-        "gene_ids", "gene_symbol", #"alias-symbol", 
-        "hgnc_symbol",
-        "NCBI_gene_id", "refseq_mrna", "uniprot_swissprot", "base_name", "has_version"
+        "gene_ids", "gene_symbol", "alias-symbol", 
+        "hgnc_symbol", "hgnc_id",
+        "NCBI_symbol", "refseq_mrna", "uniprot_swissprot",
+        "uniprot_sptrembl", "base_name", "has_version"
     ]if col in adata.var.columns]
+
     print(adata.var[new_columns]. head())
 
-
+    #Sauvegarder un nouveau fichier modifier si demander
     if args.save:
         if args.output:
             output_file=args.output
